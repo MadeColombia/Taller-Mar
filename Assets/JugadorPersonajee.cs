00ccc59a8a395308c018745ec617035816de4708
DiegoGrajales1229 <63883371+DiegoGrajales1229@users.noreply.github.com>
2020-05-10 12:49:20 -0500

    CREACION ESCENARIO PLAYA
    Se añade obj de playa, faltando el mar, se añade avatar

---
+++ Assets/JugadorPersonajee.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JugadorPersonajee : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float velocidadMovimiento,velocidadRotacion;
+    private Animator anim;
+    public float x, y ;
+    
+    void Start()
+    {
+        velocidadMovimiento = 5.0f;
+        velocidadRotacion = 200.0f;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        
+
+        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+        
+        
+    }
+}
