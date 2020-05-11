307bfe8a24f3f7cbf136b79d161346b831b00095
DiegoGrajales1229 <63883371+DiegoGrajales1229@users.noreply.github.com>
2020-05-10 19:39:19 -0500

    CAMBIOS EN PLAYA, COORDENADAS
    Coordenadas en canvas, rayo en todo

--- Assets/Scripts/guardarPosicion.cs
+++ Assets/Scripts/guardarPosicion.cs
@@ -1,33 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class guardarPosicion : MonoBehaviour
 {
     public float Posx, Posy, Posz;
-    public int tic;
-    public Timer timer;
-    public 
+    public Text tx;
+    public List<Vector3> listaPos;
+    
     void Start()
     {
-        tic = 0;
+        listaPos = new List<Vector3>();
+        
        
     }
 
     // Update is called once per frame
     void Update()
     {
- 
-            GuardarDatosPos();
+
+        
+        GuardarDatosPos();
  
     }
 
     public void GuardarDatosPos() 
+
     {
+        
         PlayerPrefs.SetFloat("PosicionX", transform.position.x);
         PlayerPrefs.SetFloat("PosicionY", transform.position.y);
         PlayerPrefs.SetFloat("PosicionZ", transform.position.z);
+        tx.text = "(X: "+PlayerPrefs.GetFloat("PosicionX").ToString() + ", Y: " + PlayerPrefs.GetFloat("PosicionY").ToString() + ", Z: " + PlayerPrefs.GetFloat("PosicionZ").ToString()+")";
+        
         Debug.Log("Datos Guardados Correctamente");
     }
 
