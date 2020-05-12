888dd2d977fe4972e59b4d2a19590775e2ef8dc0
jaimeandres77 <53912476+jaimeandres77@users.noreply.github.com>
2020-05-11 22:49:36 -0500

    CambioEntreEscenarios
    Se agrego el boton de cambio de escena en ambas escenas.

---
+++ Assets/Scripts/VolverEscenario.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VolverEscenario : MonoBehaviour
+{
+    public void volverEscena()
+    {
+        SceneManager.LoadScene("Playa");
+    }
+}
